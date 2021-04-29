1868e1054077172820d06bb9399eb9152d6010a2
Sean Taba <shahramtaba@gmail.com>
2021-04-29 08:55:14 -0600

    Worked on the custom list - finished basic functionality - did basic testing

--- src/com/revature/projects/Driver.java
+++ src/com/revature/projects/Driver.java
@@ -12,6 +12,19 @@ public class Driver {
         myList.add(50);
         myList.add(60);
 
+        myList.add(3,25);
+        myList.set(6,55);
+        System.out.println("List contains 40: " + myList.contains(40));
+        System.out.println("List contains 65: " + myList.contains(65));
+        System.out.println("List size: " + myList.size());
+        System.out.println("List is empty: " + myList.isEmpty());
+        Object[] myArray = myList.toArray();
+
+        myList.remove(5);
+        Object myInt = myList.get(3);
+        System.out.println("Index of 40: " + myList.indexOf(40));
+
+
         System.out.println(myList);
     }
 }
--- src/com/revature/projects/models/MyList.java
+++ src/com/revature/projects/models/MyList.java
@@ -1,9 +1,9 @@
 package com.revature.projects.models;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.*;
 
-public class MyList<T> {
+public class MyList<T> implements List<T> {
 
     private T[] array;
     private int numberOfElements;
@@ -24,11 +24,170 @@ public class MyList<T> {
         array = newArray;
     }
 
-    public void add(T element)
+    @Override
+    public int size() {
+        return numberOfElements;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numberOfElements == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (T t : array) {
+            if(o.equals(t)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        T[] tempArray = (T[]) new Object[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            tempArray[i] = array[i];
+        }
+        return (T[]) tempArray;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T element)
     {
         if(array.length == numberOfElements) grow();
         array[numberOfElements] = element;
         numberOfElements++;
+        return true;
+    }
+
+
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        for (T t : array) {
+            t = null;
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if(index < numberOfElements)
+        return array[index];
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        T tempT;
+        if (index < numberOfElements) {
+            tempT = array[index];
+            array[index] = element;
+            return tempT;
+        }
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index < numberOfElements)
+        {
+            if(array.length == numberOfElements) grow();
+
+            for (int i = numberOfElements; i >= index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+            numberOfElements++;
+        }
+    }
+
+    //fix the remove function
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < numberOfElements; i++) {
+            if(o.equals(array[i]))
+            {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < numberOfElements)
+        {
+            for (int i = index; i < numberOfElements - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[numberOfElements - 1] = null;
+            numberOfElements--;
+        }
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < numberOfElements; i++) {
+            if(array[i].equals(o)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
     }
 
     @Override
